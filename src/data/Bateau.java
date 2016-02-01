27c3f1fb914122b3630c100049e9fab3ae1e7d8a
Valentin ERNOUF <valentin.ernouf@me.com>
2016-02-01 16:21:14 +0100

    Ajout commentaires classe Bateau

--- src/data/Bateau.java
+++ src/data/Bateau.java
@@ -3,14 +3,25 @@ import java.awt.Point;
 
 public class Bateau {
 
+	/*
+	 Attributs de la classe
+	 */
+	
 	private int longueur;
 	private Point pos;
 	
+	/*
+	 Constructeur de la classe
+	 */
 	
 	public Bateau(int longueur, Point pos){
 		this.longueur = longueur;
 		this.pos = pos;
 	}
+	
+	/*
+	 Getters/Setters de la classe
+	 */
 	
 	public int getLongueur(){
 		return this.longueur;
