f9a02203dd451ea04af3819ba029601f2d612f58
eherbret <eherbret@INFO-SAMOROST.iut.bx1>
2016-02-01 17:20:05 +0100

    init liste bateau (finie)

--- src/services/ActionsBateau.java
+++ src/services/ActionsBateau.java
@@ -7,8 +7,15 @@ import enumeration.EnumTypeBateau;
 public class ActionsBateau {
 	public void initialiserListeBateaux(Joueur j){
 		Bateau porteAvion = new Bateau(EnumTypeBateau.PORTE_AVION);
-		
+		Bateau contreTorpilleur = new Bateau(EnumTypeBateau.CONTRE_TORPILLEUR);
+		Bateau croiseur = new Bateau(EnumTypeBateau.CROISEUR);
+		Bateau sousMarin = new Bateau(EnumTypeBateau.SOUS_MARIN);
+		Bateau torpilleur = new Bateau(EnumTypeBateau.TORPILLEUR);
 		j.getListeBateaux().add(porteAvion);
+		j.getListeBateaux().add(contreTorpilleur);
+		j.getListeBateaux().add(croiseur);
+		j.getListeBateaux().add(sousMarin);
+		j.getListeBateaux().add(torpilleur);
 	}
 	public void ajouterBateau(Joueur j, Bateau b){
 		j.getListeBateaux().add(b);
