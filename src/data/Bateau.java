7934f5407edcd8e1cd76c898f3b4506f908e8937
eherbret <eherbret@INFO-SAMOROST.iut.bx1>
2016-02-01 16:57:42 +0100

    init liste bateau + ajout bateau (version non finie)

--- src/data/Bateau.java
+++ src/data/Bateau.java
@@ -12,10 +12,16 @@ public class Bateau {
 	private EnumTypeBateau typeBateau;
 	private Point posFront;
 	private Point posBack;
+	private boolean touche;
+	private boolean coule;
 	
 	//--------------------------
 	// Constructeur de la classe
 	//--------------------------
+	
+	public Bateau(EnumTypeBateau typeBateau) {
+		this.typeBateau = typeBateau;
+	}
 	
 	public Bateau(EnumTypeBateau typeBateau, Point posFront, Point posBack){
 		this.typeBateau = typeBateau;
@@ -27,6 +33,8 @@ public class Bateau {
 	// Getters/Setters de la classe
 	//-----------------------------
 	
+	
+
 	public Point getPosFront(){
 		return this.posFront;
 	}
@@ -37,5 +45,21 @@ public class Bateau {
 
 	public Point getPosBack(){
 		return this.posBack;
+	}
+
+	public boolean isTouche() {
+		return touche;
+	}
+
+	public void setTouche(boolean touche) {
+		this.touche = touche;
+	}
+
+	public boolean isCoule() {
+		return coule;
+	}
+
+	public void setCoule(boolean coule) {
+		this.coule = coule;
 	}
 }
