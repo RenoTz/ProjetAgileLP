55b2b09f54989495277ea7f506ecf028f39b798d
eherbret <eherbret@INFO-SAMOROST.iut.bx1>
2016-02-01 16:37:10 +0100

    Ajout enumTypeBateau + modif bateau

--- src/data/Bateau.java
+++ src/data/Bateau.java
@@ -1,34 +1,36 @@
 package data;
 import java.awt.Point;
 
+import enumeration.EnumTypeBateau;
+
 public class Bateau {
 
-	/*
-	 Attributs de la classe
-	 */
+	//-----------------------
+	// Attributs de la classe
+	//-----------------------
 	
-	private int longueur;
+	private EnumTypeBateau typeBateau;
 	private Point pos;
 	
-	/*
-	 Constructeur de la classe
-	 */
+	//--------------------------
+	// Constructeur de la classe
+	//--------------------------
 	
-	public Bateau(int longueur, Point pos){
-		this.longueur = longueur;
+	public Bateau(EnumTypeBateau typeBateau, Point pos){
+		this.typeBateau = typeBateau;
 		this.pos = pos;
 	}
 	
-	/*
-	 Getters/Setters de la classe
-	 */
-	
-	public int getLongueur(){
-		return this.longueur;
-	}
+	//-----------------------------
+	// Getters/Setters de la classe
+	//-----------------------------
 	
 	public Point getPos(){
 		return this.pos;
 	}
-	
+
+	public EnumTypeBateau getTypeBateau() {
+		return typeBateau;
+	}
+
 }
---
+++ src/enumeration/EnumTypeBateau.java
@@ -0,0 +1,5 @@
+package enumeration;
+
+public enum EnumTypeBateau {
+	PORTE_AVION, CROISEUR, CONTRE_TORPILLEUR, SOUS_MARIN, TORPILLEUR
+}
