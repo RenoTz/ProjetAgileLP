e72d0c4b460da4fe15ecee47d913cb766264fc4d
mrossi002 <mrossi002@info-goox.iut.bx1>
2016-02-02 10:38:19 +0100

    Premiers pas pour les tests

--- src/Tests/Tests.java
+++ src/Tests/Tests.java
@@ -1,3 +1,17 @@
 package data;
 package enumeration;
 package services;
+
+import junit.framework.*;
+
+// Test d'un compteur de bateau
+public class LesTests {
+	
+	@Test
+    public void testCompterBateaux() {
+        //Bateau EnumTypeBateau = new Bateau();
+        //String result = bateau.concatenate(EnumTypeBateau);
+        //assertEquals("Il y a".EnumTypeBateau."bateaux", result);
+    }
+	
+}
