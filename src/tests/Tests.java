b4d6a6e72524a7a7c2c94d10efb78c84b9f89c31
jdamey <jeremy.damey@etu.u-bordeaux.fr>
2016-02-02 14:08:46 +0100

    modification test listeBateaux

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -12,20 +12,7 @@ import enumeration.EnumTypeBateau;
 import services.ActionsBateau;
 
 public class Tests {
-
-	@Test
-	public void testInitialiserListeBateaux() {
-		int nbBateaux = 0;
-		for (int cpt = 1; cpt <= 5; cpt++)
-		{
-			nbBateaux++;
-		}
-		System.out.println(EnumTypeBateau.values().length);
-		if (EnumTypeBateau.values().length != 5){
-			assertEquals("Nombre de bateaux ok", nbBateaux);
-		}
-			
-	}
+	private ActionsBateau action = new ActionsBateau();
 
 	@Test
 	public void testActionsBateauInitialiserListe() {
