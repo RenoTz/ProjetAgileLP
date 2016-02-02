94814cdaea60e2ab966f71ef9e3207963e6cddf5
jdamey <jeremy.damey@etu.u-bordeaux.fr>
2016-02-02 13:58:15 +0100

    test initialiserListeBateaux à revoir

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -1,14 +1,24 @@
 package tests;
 
 import static org.junit.Assert.*;
+import enumeration.EnumTypeBateau;
 
 import org.junit.Test;
 
 public class Tests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitialiserListeBateaux() {
+		int nbBateaux = 0;
+		for (int cpt = 1; cpt <= 5; cpt++)
+		{
+			nbBateaux++;
+		}
+		System.out.println(EnumTypeBateau.values().length);
+		if (EnumTypeBateau.values().length != 5){
+			assertEquals("Nombre de bateaux ok", nbBateaux);
+		}
+			
 	}
 
 }
