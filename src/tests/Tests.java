525bca9ef3f83ce9ee9b91077d531b3df805460b
Valentin ERNOUF <valentin.ernouf@me.com>
2016-02-02 16:57:14 +0100

    Ajout de tests des méthodes isCoule et isTouche pour tous les bateaux

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -4,15 +4,26 @@ import static org.junit.Assert.*;
 import enumeration.EnumTypeBateau;
 
 import java.util.List;
+
 import org.junit.Test;
 
 import data.bateau.Bateau;
-import enumeration.EnumTypeBateau;
+import data.bateau.ContreTorpilleur;
+import data.bateau.Croiseur;
+import data.bateau.PorteAvion;
+import data.bateau.SousMarin;
+import data.bateau.Torpilleur;
 import services.ActionsBateau;
 
 public class Tests {
 
 	private ActionsBateau action = new ActionsBateau();
+	private Croiseur croiseur;
+	private Torpilleur torpilleur;
+	private ContreTorpilleur contreTorpilleur;
+	private PorteAvion porteAvion;
+	private SousMarin sousMarin;
+
 	
 	@Test
 	public void testActionsBateauInitialiserListe() {
@@ -37,6 +48,46 @@ public class Tests {
 			}
 		}
 		return false;
+	}
+	
+	//VE
+	@Test
+	public void testIsCoule(){
+		List<Bateau> listeRetour = action.initialiserListeBateaux();
+		
+		for(Bateau bateau : listeRetour){
+			assertEquals(false, bateau.isCoule());
+		}
+	}
+	
+	@Test
+	public void testCroiseurIsTouche(){
+		croiseur = new Croiseur(EnumTypeBateau.CROISEUR);
+		assertEquals(false, croiseur.isTouche());
+	}
+	
+	@Test
+	public void testTorpilleurIsTouche(){
+		torpilleur = new Torpilleur(EnumTypeBateau.TORPILLEUR);
+		assertEquals(false, torpilleur.isTouche());
+	}
+	
+	@Test
+	public void testContreTorpilleurIsTouche(){
+		contreTorpilleur = new ContreTorpilleur(EnumTypeBateau.CONTRE_TORPILLEUR);
+		assertEquals(false, contreTorpilleur.isTouche());
+	}
+	
+	@Test
+	public void testPorteAvionIsTouche(){
+		porteAvion = new PorteAvion(EnumTypeBateau.PORTE_AVION);
+		assertEquals(false, porteAvion.isTouche());
+	}
+	
+	@Test
+	public void testSousMarinIsTouche(){
+		sousMarin = new SousMarin(EnumTypeBateau.SOUS_MARIN);
+		assertEquals(false, sousMarin.isTouche());
 	}
 
 }
