f74b7e57afad3cbf210e358f76acd7f112ac9e86
mrossi002 <mrossi002@INFO-GOO.iut.bx1>
2016-02-02 17:20:00 +0100

    Avancée de l'interface

--- src/data/interfaceJeu/Interface.java
+++ src/data/interfaceJeu/Interface.java
@@ -3,10 +3,16 @@ package data.interfaceJeu;
 import java.awt.Button;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.AbstractButton;
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -36,22 +42,28 @@ public class Interface {
 	public static void createWindow() {  
 		JFrame frame = new JFrame("Bataille navale");
 
-		JPanel panel = new JPanel();
-		panel.setLayout(new FlowLayout());
+		BorderLayout layout = new BorderLayout();
+        frame.setLayout(layout);
 
+        JButton nouvellePartieBouton = new JButton("Nouvelle Partie");
+        frame.add(nouvellePartieBouton, BorderLayout.SOUTH);
+        nouvellePartieBouton.setBackground(Color.black); //marche
+        nouvellePartieBouton.setForeground(Color.white); //marche        
+       
+		JPanel panel = new JPanel();
+		panel.setBounds(101, 650, 900, 50);
+		panel.setBackground(Color.GRAY); //background fenètre du jeu
+		
 		// Create button
 		JButton button = new JButton();
-		button.setText("Nouvelle partie");
-		button.setBackground(Color.blue);
 		
-
-		panel.add(button);
 		for (JButton bouton : listeBouton) {
 			panel.add(bouton);
 		}
 
 		frame.add(panel);
 		frame.setSize(800, 600);
+		frame.setTitle("Bataille Navale - Groupe 1");
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -63,8 +75,9 @@ public class Interface {
 			for( int j = 0; j < plateau.getLePlateau().length; j++ ) {
 				  //creer bouton + couleurs + clik dessus modifié
 				JButton button = new JButton();
-				button.setBounds(40+  j*40, 40+ i*40, 40, 40);
-				button.setBackground(Color.BLACK);
+				//button.setBounds(20+  j*20, 10+ i*20, 40, 40);
+				button.setBackground(Color.blue);
+				button.setPreferredSize(new Dimension(40, 40));//setSize(50, 50); //marche pas
 				listeBouton.add(button);
 				 
 			  }
