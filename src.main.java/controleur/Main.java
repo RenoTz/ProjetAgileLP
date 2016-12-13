c8c78a43f13f54914083052a2dc2c31ceecdc7c9
Renaud Trezieres <renaud.trezieres@capgemini.com>
2016-12-13 16:35:06 +0100

    test

--- src.main.java/controleur/Main.java
+++ src.main.java/controleur/Main.java
@@ -1,12 +1,12 @@
 package controleur;
 
 public class Main {
-    
+
     @SuppressWarnings("unused")
     public static void main(String[] args) {
-        
+
         Partie partie = new Partie();
-        
-        // test
+
+        // testzezeze
     }
 }
